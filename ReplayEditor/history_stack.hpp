7a8861deabe7eaa0d49cacf5dedd3126b07a6429
thebetioplane <thebetioplane@users.noreply.github.com>
2023-07-07 19:05:29 -0400

    Fixed bug with history stack where it would switch to the wrong state

--- ReplayEditor/history_stack.hpp
+++ ReplayEditor/history_stack.hpp
@@ -6,30 +6,24 @@
 #include <array>
 #include <memory>
 
-template <typename T, size_t MAX_HISTORY>
+template <typename T, int MAX_HISTORY>
 class HistoryStack
 {
-   private:
-    std::array<std::unique_ptr<T>, MAX_HISTORY> m_arr;
-    size_t m_index = 0;
-    size_t m_undoable_count = 0;
-    size_t m_redoable_count = 0;
-
    public:
     const T *current() const
     {
-        return m_arr[(m_index - 1) % MAX_HISTORY].get();
+        return m_arr[index_minus_one()].get();
     }
 
     T *current()
     {
-        return m_arr[(m_index - 1) % MAX_HISTORY].get();
+        return m_arr[index_minus_one()].get();
     }
 
     void push_mutation(std::unique_ptr<T> ptr)
     {
         m_arr[m_index] = std::move(ptr);
-        m_index = (m_index + 1) % MAX_HISTORY;
+        m_index = index_plus_one();
         if (m_undoable_count < MAX_HISTORY) ++m_undoable_count;
         m_redoable_count = 0;
     }
@@ -40,7 +34,7 @@ class HistoryStack
         if (m_undoable_count > 1) {
             --m_undoable_count;
             ++m_redoable_count;
-            m_index = (m_index - 1) % MAX_HISTORY;
+            m_index = index_minus_one();
             return true;
         }
         return false;
@@ -49,11 +43,31 @@ class HistoryStack
     bool redo_mutation()
     {
         if (m_redoable_count > 0) {
-            m_index = (m_index + 1) % MAX_HISTORY;
+            m_index = index_plus_one();
             --m_redoable_count;
             ++m_undoable_count;
             return true;
         }
         return false;
     }
+
+   private:
+    int index_plus_one() const
+    {
+        return (m_index + 1) % MAX_HISTORY;
+    }
+
+    int index_minus_one() const
+    {
+        if (m_index <= 0) {
+            return MAX_HISTORY - 1;
+        } else {
+            return m_index - 1;
+        }
+    }
+
+    std::array<std::unique_ptr<T>, MAX_HISTORY> m_arr;
+    int m_index = 0;
+    int m_undoable_count = 0;
+    int m_redoable_count = 0;
 };
